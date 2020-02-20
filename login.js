f6aab17a0bfda4d2f05b023b915f55e1fa94ee30
dbarrinha <dbarrinha@hotmail.com>
2020-02-20 09:38:09 -0300

    :art: Creating SignIn Structure

---
+++ login.js
@@ -0,0 +1,46 @@
+
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+module.exports = () => {
+  const router = new SignUpRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(router))
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
+
+class SignUpRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = new SignUseCase().signUp(email, password, repeatPassword)
+    return {
+      statusCode: 200,
+      body: user
+    }
+  }
+}
+
+class SignUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
+}
