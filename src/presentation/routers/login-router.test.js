1575d48ae36ccccee3255e7343263caa98921633
dbarrinha <dbarrinha@hotmail.com>
2020-02-21 14:59:29 -0300

    :white_check_mark: Ensure LoginRouter return 400 if no password is provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   async route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400,
         body: {}
@@ -15,6 +15,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httResponse = await sut.route(httpRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
       }
     }
     const httResponse = await sut.route(httpRequest)
