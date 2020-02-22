8b2510c79a163c774285621a7499d1bda306ae6e
unknown <dbarrinha@hotmail.com>
2020-02-22 18:11:34 -0300

    :art: Move classes to your respectives directorys

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,11 +1,15 @@
+const HttpResponse = require('../helpers/http-response')
 module.exports = class LoginRouter {
   async route (httpRequest) {
-    /* const { email, password, repeatPassword } = httpRequest.body
-        const user = new SignUseCase().signUp(email, password, repeatPassword)
-        */
-    return {
-      statusCode: 400,
-      body: {}
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
+    }
+    const { email, password } = httpRequest.body
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+    if (!password) {
+      return HttpResponse.badRequest('password')
     }
   }
 }
